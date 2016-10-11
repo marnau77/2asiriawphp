405bcdfa4e1a4e94a79763589f8b25057a2ee909
marnau <marnau@marnau>
2016-10-11 21:20:01 +0200

    Incorporamos ejemplo

--- index.php
+++ index.php
@@ -1,19 +1,10 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+<!DOCTYPE html>
 <html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Insert title here</title>
-</head>
-<frameset>
-    <frame>
-    <frame>
-    <noframes>
-    <body>
-    <p>This page uses frames. The current browser you are using does not support frames.</p>
-    <?php
-    echo "My first PHP scriptsss!";
-	?>
-    </body>
-    </noframes>
-</frameset>
-</html>
+<body>
+
+<?php
+echo "My first PHP script!";
+?>
+
+</body>
+</html> 
